372787c35e2d2e103f0f6796e3fc401f9b198b1c
DGWind0611 <qingrong.z0611@gmail.com>
2023-03-06 22:37:08 +0800

    v1.1

--- animate.js
+++
@@ -1,5 +0,0 @@
-// 在DOM元素加載完成後，使用JavaScript將每個字元包裹在一個 <span> 元素中
-document.addEventListener('DOMContentLoaded', function() {
-    let myText = document.getElementById('myText');
-    myText.innerHTML = myText.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-});
